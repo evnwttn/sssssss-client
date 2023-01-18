efa055dfae719e0c207e334e5a821904efb0ebdc
evnwttn <evnwttn@gmail.com>
2023-01-18 15:33:54 -0330

    Update BigShinyButton.tsx

--- app/src/components/BigShinyButton.tsx
+++ app/src/components/BigShinyButton.tsx
@@ -1,14 +1,14 @@
+import axios from "axios";
 import { Box, Button } from "@mui/material";
 import { BSBButton } from "../types/BSBButton";
 import { bsbSx } from "../styles/inline";
 
 export const BigShinyButton = ({ button }: BSBButton) => {
-  const axios = require("axios").default;
-
   const onButtonClick = (button: string) => {
     axios
-      .put(`http://localhost:8000/`, {
-        button,
+      .put("http://localhost:8000/", button)
+      .then(function (response: any) {
+        console.log(response);
       })
       .catch(function (error: any) {
         console.log(error);
